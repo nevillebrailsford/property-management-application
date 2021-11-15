9ba7420266a1071e7b642cbada0098af6bd37bed
Neville Brailsford <neville_brailsford@hotmail.com>
2021-11-15 17:19:24 +0000

    Added FileHandler, added reset button action and added millis to timestamps

--- src/main/java/com/brailsoft/property/management/logging/PropertyManagerLogConfigurer.java
+++ src/main/java/com/brailsoft/property/management/logging/PropertyManagerLogConfigurer.java
@@ -1,16 +1,21 @@
 package com.brailsoft.property.management.logging;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.brailsoft.property.management.constant.Constants;
+import com.brailsoft.property.management.persistence.LocalStorage;
 import com.brailsoft.property.management.preference.ApplicationPreferences;
 
 public class PropertyManagerLogConfigurer {
 	private static final Logger LOGGER = Logger.getLogger(Constants.LOGGER_NAME);
 	private static ApplicationPreferences preferences = ApplicationPreferences.getInstance(Constants.NODE_NAME);
+	private static final String LOG_FILE = "property.manager.log";
 
 	public static void setUp() {
 		Level loggerLevel = preferences.getLevel();
@@ -28,6 +33,19 @@ public class PropertyManagerLogConfigurer {
 		handler.setLevel(loggerLevel);
 		handler.setFormatter(new PropertyManagerFormatter());
 		LOGGER.addHandler(handler);
+
+		try {
+			String rootDirectory = preferences.getDirectory() + File.separator + LocalStorage.DIRECTORY;
+			String logfileName = rootDirectory + File.separator + LOG_FILE;
+			FileHandler fileHandler = new FileHandler(logfileName, false);
+			fileHandler.setLevel(loggerLevel);
+			fileHandler.setFormatter(new PropertyManagerFormatter());
+			LOGGER.addHandler(fileHandler);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		LOGGER.setLevel(loggerLevel);
 	}
 }
